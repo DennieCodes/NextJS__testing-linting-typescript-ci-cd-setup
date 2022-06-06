1152dab241fe1aa7dccdcf016f5f707d072bdd04
Oculareo <oculareo@gmail.com>
2022-06-05 21:34:52 -0400

    Added and configured React-Testing-Library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.jest.json',
+		},
+	},
+	setupFilesAfterEnv: ['./src/jest.setup.ts'],
 };
