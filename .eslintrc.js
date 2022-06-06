4815c7afd4618959ab3575ebe0485c50a5d3cd5b
Oculareo <oculareo@gmail.com>
2022-06-06 09:56:15 -0400

    Added ESlint and configured for Jest

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:jest/recommended',
+		'plugin:jest/style',
+		'next',
+		'next/core-web-vitals',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react', '@typescript-eslint'],
+
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+			// As mentioned in the comments, you should extend TypeScript plugins here,
+			// instead of extending them outside the `overrides`.
+			// If you don't want to extend any rules, you don't need an `extends` attribute.
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/eslint-recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+			],
+			rules: {
+				'@typescript-eslint/require-await': 'error',
+			},
+
+			parserOptions: {
+				project: ['./tsconfig.json'],
+				ecmaFeatures: {
+					jsx: true,
+				},
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+	],
+
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
+};
