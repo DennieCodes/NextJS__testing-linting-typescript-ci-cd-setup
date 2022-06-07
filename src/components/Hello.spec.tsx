a63ef4765883f376c0c3c6cc0417b7bcbfeaf09e
Oculareo <oculareo@gmail.com>
2022-06-07 13:58:05 -0400

    Removed temporary testing components

--- src/components/Hello.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Hello } from './Hello';
-
-it('renders "Hello"', () => {
-	render(<Hello />);
-	const myElement = screen.getByText('Hello');
-	expect(myElement).toBeInTheDocument();
-});
