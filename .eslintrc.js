08b9ca9654826bda4832b70ef8b3f0ee102b3bd7
Oculareo <oculareo@gmail.com>
2022-06-07 09:46:54 -0400

    Adjusted ESLint settings for troubleshooting actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,49 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:jest/recommended',
-		'plugin:jest/style',
-		'plugin:testing-library/react',
-		'next',
-		'next/core-web-vitals',
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react', '@typescript-eslint'],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:testing-library/react",
+    "eslint:recommended",
+    "next",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
 
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
-	overrides: [
-		{
-			files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
 
-			// As mentioned in the comments, you should extend TypeScript plugins here,
-			// instead of extending them outside the `overrides`.
-			// If you don't want to extend any rules, you don't need an `extends` attribute.
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-				'plugin:@typescript-eslint/eslint-recommended',
-				'plugin:@typescript-eslint/recommended-requiring-type-checking',
-			],
-			rules: {
-				'@typescript-eslint/require-await': 'error',
-			},
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      rules: {
+        "@typescript-eslint/require-await": "error",
+      },
 
-			parserOptions: {
-				project: ['./tsconfig.json'],
-				ecmaFeatures: {
-					jsx: true,
-				},
-				ecmaVersion: 'latest',
-				sourceType: 'module',
-			},
-		},
-	],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  ],
 
-	rules: {
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-	},
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
 };
